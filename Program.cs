14df9dd5807bf82bf98b31fe216d8847d6a08957
Henrique Pereira <henriquepsz@protonmail.com>
2024-01-27 17:00:40 -0300

    Configured Program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,34 @@
+using System.Text;
+using Amazon.CognitoIdentityProvider;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddAWSService<IAmazonCognitoIdentityProvider>();
+
+// Adicione serviços de autenticação
+builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+    .AddJwtBearer(options =>
+    {
+        options.RequireHttpsMetadata = false;
+        options.SaveToken = true;
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes($"{builder.Configuration["AWS:CognitoSecret"]}")),
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidIssuer = $"https://cognito-idp.{builder.Configuration["AWS:Region"]}.amazonaws.com/{builder.Configuration["AWS:UserPoolId"]}",
+            ValidAudience = builder.Configuration["AWS:AppClientId"]
+        };
+    });
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
